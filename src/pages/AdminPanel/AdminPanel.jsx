f901c23b4e3831e1bc8aac109ccda28def9a9941
Phantather <akdil.hstech@gmail.com>
2022-11-23 13:58:07 +0600

    control push

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import AdminFilms from "./AdminFilms/AdminFilms";
+import AdminUsers from "./AdminUsers/AdminUsers";
 
 const AdminPanel = () => {
 
@@ -25,118 +27,10 @@ const AdminPanel = () => {
             </aside>
             {
                 filmsShow === 'films' ?
-                    <div className="admin__info">
-                        <h2 className="admin__info-title">
-                            Все филмы
-                        </h2>
-                        <table className="admin__table" >
-                            <tr className="admin__table-block">
-                                <th className="admin__table-title">
-                                    ID
-                                </th>
-                                <th className="admin__table-title">
-                                    Название
-                                </th>
-                                <th className="admin__table-title">
-                                    Год
-                                </th>
-                                <th className="admin__table-title">
-                                    Жанр
-                                </th>
-                                <th className="admin__table-title">
-                                    Статус
-                                </th>
-                                <th className="admin__table-title">
-                                    Изменить
-                                </th>
-                                <th className="admin__table-title">
-                                    Удалить
-                                </th>
-                            </tr>
-                            <tr className="admin__table-block">
-                                <td className="admin__table-info">
-                                    1
-                                </td>
-                                <td className="admin__table-info">
-                                    Человек-паук: Нет пути домой
-                                </td>
-                                <td className="admin__table-info">
-                                    2021
-                                </td>
-                                <td className="admin__table-info">
-                                    Приключения
-                                </td>
-                                <td className="admin__table-info">
-                                    Free
-                                </td>
-                                <td className="admin__table-info">
-                                    <button className="admin__table-btn">
-                                        Изменить
-                                    </button>
-                                </td>
-                                <td className="admin__table-info">
-                                    <button className="admin__table-btn">
-                                        X
-                                    </button>
-                                </td>
-                            </tr>
-                        </table >
-                    </div>
+                    <AdminFilms/>
                     :
-                    <div className="admin__info">
-                        <h2 className="admin__info-title">
-                            Пользователи
-                        </h2>
-                        <table className="admin__table" >
-                            <tr className="admin__table-block">
-                                <th className="admin__table-title">
-                                    ID
-                                </th>
-                                <th className="admin__table-title">
-                                    Login
-                                </th>
-                                <th className="admin__table-title">
-                                    Email
-                                </th>
-                                <th className="admin__table-title">
-                                    Номер телефона
-                                </th>
-                                <th className="admin__table-title">
-                                    Изменить
-                                </th>
-                                <th className="admin__table-title">
-                                    Удалить
-                                </th>
-                            </tr>
-                            <tr className="admin__table-block">
-                                <td className="admin__table-info">
-                                    1
-                                </td>
-                                <td className="admin__table-info">
-                                    Muslim
-                                </td>
-                                <td className="admin__table-info">
-                                    muslim@mail.ru
-                                </td>
-                                <td className="admin__table-info">
-                                    +996555555555
-                                </td>
-
-                                <td className="admin__table-info">
-                                    <button className="admin__table-btn">
-                                        Изменить
-                                    </button>
-                                </td>
-                                <td className="admin__table-info">
-                                    <button className="admin__table-btn">
-                                        X
-                                    </button>
-                                </td>
-                            </tr>
-                        </table >
-                    </div>
+                    <AdminUsers/>
             }
-
         </section>
     );
 };
